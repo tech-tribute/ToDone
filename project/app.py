be494005eb11175d41a60539a6423ce79c66b8a2
Alii <aliireza.dev@gmail.com>
2023-11-23 18:55:17 +0330

    Not found 403 bug (while packaging)

--- project/app.py
+++ project/app.py
@@ -1,83 +1,10 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, String, Integer, DateTime
-from datetime import datetime
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite.db"
 db = SQLAlchemy(app)
-
-
-class Task(db.Model):
-    __tablename__ = "tasks"
-    id = Column(Integer(), nullable=False, primary_key=True)
-    caption = Column(String(128), nullable=False)
-    create = Column(DateTime(), default=datetime.now, nullable=False)
-
-
-@app.before_request
-def dbInit():
-    db.create_all()
-
-
-@app.route("/", methods=("POST", "GET"))
-def index():
-    if request.method == "POST":
-        form = request.form.to_dict()
-        caption = form["caption"]
-
-        # validation
-        if not caption:
-            return "You must enter something for caption!"
-
-        try:
-            new_task = Task(caption=caption)
-            db.session.add(new_task)
-            db.session.commit()
-        except:
-            return "something went wrong! (During adding your task)"
-
-        return redirect("/")
-    elif request.method == "GET":
-        tasks = Task.query.all()
-
-    return render_template("index/index.html", tasks=tasks)
-
-
-@app.route("/delete/<int:id>")
-def deleteTask(id):
-    task = Task.query.get_or_404(id)
-
-    try:
-        db.session.delete(task)
-        db.session.commit()
-    except:
-        return f"Something went wrong during deleting the task id:{id}"
-
-    return redirect("/")
-
-
-@app.route("/update/<int:id>", methods=("POST", "GET"))
-def updateTask(id):
-    task = Task.query.get_or_404(id)
-
-    if request.method == "POST":
-        form = request.form.to_dict()
-        new_caption = form["caption"]
-
-        if not new_caption:
-            return "You must enter something for caption!"
-
-        task.caption = new_caption
-
-        try:
-            db.session.commit()
-            return redirect("/")
-        except:
-            return f"Something went wrong during updating the task id:{id}"
-
-    return render_template("index/update.html", task=task)
 
 
 if __name__ == "__main__":
---
+++ project/todone/models.py
@@ -0,0 +1,10 @@
+from app import db
+from sqlalchemy import Column, String, Integer, DateTime
+from datetime import datetime
+
+
+class Task(db.Model):
+    __tablename__ = "tasks"
+    id = Column(Integer(), nullable=False, primary_key=True)
+    caption = Column(String(128), nullable=False)
+    create = Column(DateTime(), default=datetime.now, nullable=False)
