359624f100f7d3d72158da76c7151f9df60958d9
Alii <aliireza.dev@gmail.com>
2023-11-23 16:30:55 +0330

    database operetional error solved | update/delete ability added.

--- project/app.py
+++ project/app.py
@@ -16,6 +16,11 @@ class Task(db.Model):
     create = Column(DateTime(), default=datetime.now, nullable=False)
 
 
+@app.before_request
+def dbInit():
+    db.create_all()
+
+
 @app.route("/", methods=("POST", "GET"))
 def index():
     if request.method == "POST":
@@ -36,8 +41,43 @@ def index():
         return redirect("/")
     elif request.method == "GET":
         tasks = Task.query.all()
-        
+
     return render_template("index/index.html", tasks=tasks)
+
+
+@app.route("/delete/<int:id>")
+def deleteTask(id):
+    task = Task.query.get_or_404(id)
+
+    try:
+        db.session.delete(task)
+        db.session.commit()
+    except:
+        return f"Something went wrong during deleting the task id:{id}"
+
+    return redirect("/")
+
+
+@app.route("/update/<int:id>", methods=("POST", "GET"))
+def updateTask(id):
+    task = Task.query.get_or_404(id)
+
+    if request.method == "POST":
+        form = request.form.to_dict()
+        new_caption = form["caption"]
+
+        if not new_caption:
+            return "You must enter something for caption!"
+
+        task.caption = new_caption
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return f"Something went wrong during updating the task id:{id}"
+
+    return render_template("index/update.html", task=task)
 
 
 if __name__ == "__main__":
