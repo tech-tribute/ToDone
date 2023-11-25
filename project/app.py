82f75cd3d8cde56fe3f86831ab7e3eeb2f0b7461
Alii <aliireza.dev@gmail.com>
2023-11-25 20:21:25 +0330

    db migration instead of db.create_all()

--- project/app.py
+++ project/app.py
@@ -3,6 +3,13 @@ from os import path
 
 
 if __name__ == "__main__":
-    with app.app_context(): 
-        db.create_all()
+    # with app.app_context(): 
+    #     db.create_all()
+
+    # during development
+    # instead of code above run these codes below in terminal :
+    # >>> flask db init
+    # >>> flask db migrate
+    # >>> flask db upgrade
+    
     app.run(debug=True, port=800)
--- project/todone/__init__.py
+++ project/todone/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from todone.config import Config, Development
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Development)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 from todone import router
