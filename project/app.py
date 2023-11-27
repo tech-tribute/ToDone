d8eece15b7bbdd59488224cc1615d84323e97237
Alii <aliireza.dev@gmail.com>
2023-11-27 11:07:33 +0330

    update app.py

--- project/app.py
+++ project/app.py
@@ -1,11 +1,7 @@
-from todone import app, db
-from os import path
+from todone import app
 
 
 if __name__ == "__main__":
-    # with app.app_context(): 
-    #     db.create_all()
-
     # during development
     # instead of code above run these codes below in terminal :
     # >>> flask db init
