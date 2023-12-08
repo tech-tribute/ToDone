96a147b76fe513576c6037ec84d0cc17b1857ecf
Alii <aliireza.dev@gmail.com>
2023-12-08 11:23:17 +0330

    todo route and its template, static added

--- project/todone/routers.py
+++ project/todone/routers.py
@@ -6,3 +6,8 @@ from flask import render_template
 @app.route("/",)
 def index():
     return render_template("index.html")
+
+
+@app.route("/todo/")
+def todo():
+    return render_template("todo.html")
